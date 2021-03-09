7db5065b32e5eb0571855826d943b11b2def68fd
Fakhri Yusifli <fakhriyusifli@gmail.com>
2021-03-09 23:28:52 +0400

    axios instances

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from '../../axios';
 
 class Blog extends Component {
     state = {
